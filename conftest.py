598014c2e144efb9efd05b2f36d04c02e8e88000
Marcin Ziółkowski <djziootek@o2.pl>
2026-05-15 10:54:00 +0200

    Make screenshot hook safe

--- conftest.py
+++ conftest.py
@@ -1,6 +1,5 @@
 import os
 import pytest
-import allure
 
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
@@ -36,33 +35,19 @@ def driver(request):
 
 
 @pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item):
-
+def pytest_runtest_makereport(item, call):
     outcome = yield
     report = outcome.get_result()
 
-    # screenshot tylko przy failu testu
     if report.when == "call" and report.failed:
-
         driver = getattr(item, "driver", None)
 
         if driver:
-
             os.makedirs("screenshots", exist_ok=True)
+            screenshot_path = f"screenshots/{item.name}.png"
 
-            screenshot_path = (
-                f"screenshots/{item.name}.png"
-            )
-
-            driver.save_screenshot(screenshot_path)
-
-            # attach do Allure
-            with open(screenshot_path, "rb") as image_file:
-
-                allure.attach(
-                    image_file.read(),
-                    name=item.name,
-                    attachment_type=allure.attachment_type.PNG
-                )
-
-            print(f"\nScreenshot saved: {screenshot_path}")
+            try:
+                driver.save_screenshot(screenshot_path)
+                print(f"\nScreenshot saved: {screenshot_path}")
+            except Exception as e:
+                print(f"\nCould not save screenshot: {e}")
